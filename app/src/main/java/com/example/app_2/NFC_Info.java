b2a5234368baaba24f667313880faa3b5cf32dae
SaadSatter <saadsatter@gmail.com>
2021-06-03 03:50:41 -0400

    Activity Frames

---
+++ app/src/main/java/com/example/app_2/NFC_Info.java
@@ -0,0 +1,21 @@
+package com.example.app_2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class NFC_Info extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nfc_info);
+    }
+
+    public void Back_NFC_Dectector (View V){
+        Intent newIntent = new Intent(this, MainActivity.class);
+        startActivity(newIntent);
+    }
+}
