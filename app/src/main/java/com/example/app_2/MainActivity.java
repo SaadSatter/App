adc4b2cc49eb1f4bbfc70f90bf99ba637af30afc
Saad Satter <saadsatter@gmail.com>
2021-06-25 14:34:09 -0400

    NFC Detection Works

--- app/src/main/java/com/example/app_2/MainActivity.java
+++ app/src/main/java/com/example/app_2/MainActivity.java
@@ -2,20 +2,68 @@ package com.example.app_2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.PendingIntent;
 import android.content.Intent;
+import android.nfc.NfcAdapter;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    //Intialize attributes
+    NfcAdapter nfcAdapter;
+    PendingIntent pendingIntent;
+    final static String TAG = "nfc_test";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Initialise NfcAdapter
+        nfcAdapter = NfcAdapter.getDefaultAdapter(this);
+        //If no NfcAdapter, display that the device has no NFC
+        if (nfcAdapter == null){
+            Toast.makeText(this,"NO NFC Capabilities",
+                    Toast.LENGTH_SHORT).show();
+            finish();
+        }
+        //Create a PendingIntent object so the Android system can
+        //populate it with the details of the tag when it is scanned.
+        //PendingIntent.getActivity(Context,requestcode(identifier for
+        //                           intent),intent,int)
+        pendingIntent = PendingIntent.getActivity(this,0,new Intent(this,this.getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP),0);
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        assert nfcAdapter != null;
+        //nfcAdapter.enableForegroundDispatch(context,pendingIntent,
+        //                                    intentFilterArray,
+        //                                    techListsArray)
+        nfcAdapter.enableForegroundDispatch(this,pendingIntent,null,null);
+    }
+    protected void onPause() {
+        super.onPause();
+        //Onpause stop listening
+        if (nfcAdapter != null) {
+            nfcAdapter.disableForegroundDispatch(this);
+        }
+    }
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        setIntent(intent);
+        resolveIntent(intent);
     }
 
-    public void NFC_Dectector (View V){
-        Intent newIntent = new Intent(this, NFC_Info.class);
-        startActivity(newIntent);
+    private void resolveIntent(Intent intent) {
+        String action = intent.getAction();
+        if (NfcAdapter.ACTION_TAG_DISCOVERED.equals(action)
+                || NfcAdapter.ACTION_TECH_DISCOVERED.equals(action)
+                || NfcAdapter.ACTION_NDEF_DISCOVERED.equals(action)) {
+            Intent intent1 = new Intent(this, NFC_Info.class);
+            startActivity(intent1);
+        }
     }
+
 }
