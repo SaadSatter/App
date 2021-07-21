51fb1ba19dd0cff3de02f290dba06f065d7c4cab
Saad Satter <saadsatter@gmail.com>
2021-07-21 15:09:16 -0400

    Reads NDEF messages now

--- app/src/main/java/com/example/app_2/MainActivity.java
+++ app/src/main/java/com/example/app_2/MainActivity.java
@@ -8,10 +8,17 @@ import android.nfc.NfcAdapter;
 import android.nfc.Tag;
 import android.nfc.tech.MifareClassic;
 import android.nfc.tech.MifareUltralight;
+import android.nfc.NdefMessage;
+import android.nfc.NdefRecord;
+import android.nfc.NfcAdapter;
+import android.nfc.tech.NfcV;
 import android.os.Bundle;
+import android.os.Parcelable;
 import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
+
+import java.io.UnsupportedEncodingException;
 
 public class MainActivity extends AppCompatActivity {
     //Intialize attributes
@@ -25,11 +32,11 @@ public class MainActivity extends AppCompatActivity {
         //Initialise NfcAdapter
         nfcAdapter = NfcAdapter.getDefaultAdapter(this);
         //If no NfcAdapter, display that the device has no NFC
-        /*if (nfcAdapter == null){
+        if (nfcAdapter == null){
             Toast.makeText(this,"NO NFC Capabilities",
                     Toast.LENGTH_SHORT).show();
             finish();
-        }*/
+        }
         //Create a PendingIntent object so the Android system can
         //populate it with the details of the tag when it is scanned.
         //PendingIntent.getActivity(Context,requestcode(identifier for
@@ -37,12 +44,6 @@ public class MainActivity extends AppCompatActivity {
         pendingIntent = PendingIntent.getActivity(this,0,new Intent(this,this.getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP),0);
     }
 
-    public void NFC_Dectector (View V){
-        Intent newIntent = new Intent(this, NFC_Info.class);
-        startActivity(newIntent);
-    }
-
-/*
     @Override
     protected void onResume() {
         super.onResume();
@@ -69,18 +70,47 @@ public class MainActivity extends AppCompatActivity {
     private void resolveIntent(Intent intent) {
         String action = intent.getAction();
         if (
-                NfcAdapter.ACTION_TAG_DISCOVERED.equals(action)
-                || NfcAdapter.ACTION_TECH_DISCOVERED.equals(action)
-                || NfcAdapter.ACTION_NDEF_DISCOVERED.equals(action)) {
+                //NfcAdapter.ACTION_TAG_DISCOVERED.equals(action) ||
+                         //NfcAdapter.ACTION_TECH_DISCOVERED.equals(action) ||
+                         NfcAdapter.ACTION_NDEF_DISCOVERED.equals(action)) {
             Tag tag = (Tag) intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
             assert tag != null;
             byte[] payload = detectTagData(tag).getBytes();
-            //Open the next activity
+            Parcelable[] rawMsgs = intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES);
+            NdefMessage[] msgs = null;
+            if (rawMsgs != null) {
+                msgs = new NdefMessage[rawMsgs.length];
+                for (int i = 0; i < rawMsgs.length; i++) {
+                    msgs[i] = (NdefMessage) rawMsgs[i];
+                }
+            }
+            buildTagViews(msgs);
             Intent intent1 = new Intent(this, NFC_Info.class);
             startActivity(intent1);
         }
     }
-*/
+    private void buildTagViews(NdefMessage[] msgs) {
+        if (msgs == null || msgs.length == 0) return;
+
+        String text = "";
+//        String tagId = new String(msgs[0].getRecords()[0].getType());
+        byte[] payload = msgs[0].getRecords()[0].getPayload();
+        String textEncoding = ((payload[0] & 128) == 0) ? "UTF-8" : "UTF-16"; // Get the Text Encoding
+        int languageCodeLength = payload[0] & 0063; // Get the Language Code, e.g. "en"
+        // String languageCode = new String(payload, 1, languageCodeLength, "US-ASCII");
+
+        try {
+            // Get the Text
+            text = new String(payload, languageCodeLength, payload.length - languageCodeLength , textEncoding);
+        } catch (UnsupportedEncodingException e) {
+            Log.e("UnsupportedEncoding", e.toString());
+        }
+        Log.v("puppy", "NFC Content: " + text);
+        //tvNFCContent.setText("NFC Content: " + text);
+    }
+
+
+//MIFARE SHIT
     private String detectTagData(Tag tag) {
         StringBuilder sb = new StringBuilder();
         byte[] id = tag.getId();
@@ -99,60 +129,61 @@ public class MainActivity extends AppCompatActivity {
         sb.delete(sb.length() - 2, sb.length());
 
         for (String tech : tag.getTechList()) {
-            if (tech.equals(MifareClassic.class.getName())) {
+            if (tech.equals(NfcV.class.getName())) {
                 sb.append('\n');
                 String type = "Unknown";
 
                 try {
-                    MifareClassic mifareTag = MifareClassic.get(tag);
+                    NfcV nfcVTag = NfcV.get(tag);
 
-                    switch (mifareTag.getType()) {
-                        case MifareClassic.TYPE_CLASSIC:
-                            type = "Classic";
-                            break;
-                        case MifareClassic.TYPE_PLUS:
-                            type = "Plus";
-                            break;
-                        case MifareClassic.TYPE_PRO:
-                            type = "Pro";
-                            break;
-                    }
-                    sb.append("Mifare Classic type: ");
-                    sb.append(type);
+//                    switch (nfcVTag.getType()) {
+//                        case NfcV.TYPE_CLASSIC:
+//                            type = "Classic";
+//                            break;
+//                        case MifareClassic.TYPE_PLUS:
+//                            type = "Plus";
+//                            break;
+//                        case MifareClassic.TYPE_PRO:
+//                            type = "Pro";
+//                            break;
+//                    }
+//                    sb.append("Mifare Classic type: ");
+//                    sb.append(type);
+//                    sb.append('\n');
+
+                    sb.append("NfcV DfsID: ");
+                    sb.append(nfcVTag.getDsfId() + " bytes");
                     sb.append('\n');
 
-                    sb.append("Mifare size: ");
-                    sb.append(mifareTag.getSize() + " bytes");
+                    sb.append("NfcV Flags: ");
+                    sb.append(nfcVTag.getResponseFlags());
                     sb.append('\n');
 
-                    sb.append("Mifare sectors: ");
-                    sb.append(mifareTag.getSectorCount());
+                    sb.append("NfcV Transceive Length: ");
+                    sb.append(nfcVTag.getMaxTransceiveLength());
                     sb.append('\n');
-
-                    sb.append("Mifare blocks: ");
-                    sb.append(mifareTag.getBlockCount());
                 } catch (Exception e) {
                     sb.append("Mifare classic error: " + e.getMessage());
                 }
             }
 
-            if (tech.equals(MifareUltralight.class.getName())) {
-                sb.append('\n');
-                MifareUltralight mifareUlTag = MifareUltralight.get(tag);
-                String type = "Unknown";
-                switch (mifareUlTag.getType()) {
-                    case MifareUltralight.TYPE_ULTRALIGHT:
-                        type = "Ultralight";
-                        break;
-                    case MifareUltralight.TYPE_ULTRALIGHT_C:
-                        type = "Ultralight C";
-                        break;
-                }
-                sb.append("Mifare Ultralight type: ");
-                sb.append(type);
-            }
+//            if (tech.equals(MifareUltralight.class.getName())) {
+//                sb.append('\n');
+//                MifareUltralight mifareUlTag = MifareUltralight.get(tag);
+//                String type = "Unknown";
+//                switch (mifareUlTag.getType()) {
+//                    case MifareUltralight.TYPE_ULTRALIGHT:
+//                        type = "Ultralight";
+//                        break;
+//                    case MifareUltralight.TYPE_ULTRALIGHT_C:
+//                        type = "Ultralight C";
+//                        break;
+//                }
+//                sb.append("Mifare Ultralight type: ");
+//                sb.append(type);
+//            }
         }
-        Log.v("test",sb.toString());
+        Log.v("poppy",sb.toString());
         return sb.toString();
     }
     private String toHex(byte[] bytes) {
